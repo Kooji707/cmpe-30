1afaca416be0c4c49de7da3455b625125b0a74c8
Kooji707 <bboykooji@gmail.com>
2024-02-06 11:26:25 -0800

    convert double to integer for final

--- week3/grosssalary.c
+++ week3/grosssalary.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 int main (){
+
     double basic_salary, gross_salary;
     double medical = 1000;
     double insurance = 2000;
@@ -7,7 +8,9 @@ int main (){
     scanf("%lf",&basic_salary);
     gross_salary = ((basic_salary) + (basic_salary * 0.2) + (basic_salary * 0.4) + (medical) - (basic_salary * 0.1) - (insurance) - (basic_salary * 0.1));
 
-    printf("The Gross Salary is %.2f", gross_salary);
+    int gross_salary_integer = gross_salary;
+    printf("The Gross Salary is %d", gross_salary_integer);
 
     return 0;
+    
 }
