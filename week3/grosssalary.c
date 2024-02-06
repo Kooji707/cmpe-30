64ec5d874acf408892538e8088d3a1f9258dedbe
Kooji707 <bboykooji@gmail.com>
2024-02-06 11:17:32 -0800

    addition mistake correction

--- week3/grosssalary.c
+++ week3/grosssalary.c
@@ -5,7 +5,7 @@ int main (){
     double insurance = 2000;
     printf("Enter your yearly basis salary: ");
     scanf("%lf",&basic_salary);
-    gross_salary = ((basic_salary * (0.2)) - medical - insurance);
+    gross_salary = ((basic_salary) + (basic_salary * 0.2) + (basic_salary * 0.4) + (medical) - (basic_salary * 0.1) - (insurance) - (basic_salary * 0.1));
 
     printf("The Gross Salary is %lf", gross_salary);
 
