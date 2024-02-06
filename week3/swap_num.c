6f5e1719288c03470ff6279e75a2338a63bd4eaa
Kooji707 <bboykooji@gmail.com>
2024-02-06 11:03:57 -0800

    week3 swap variables program

---
+++ week3/swap_num.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main (){
+    int a = 30;
+    int b = 90;
+
+    printf("Before a = %d and b = %d\n", a, b);
+
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    printf("After a = %d and b = %d", a, b);
+
+    return 0;
+}
