b298281911157cfc08c1ef7cd483aab45be2fa25
Kooji707 <bboykooji@gmail.com>
2024-02-07 22:25:00 -0800

    Calculations corrected

--- week3/grosssalary.c
+++ week3/grosssalary.c
@@ -6,7 +6,13 @@ int main (){
     double insurance = 2000;
     printf("Enter your yearly basis salary: ");
     scanf("%lf",&basic_salary);
-    gross_salary = ((basic_salary) + (basic_salary * 0.2) + (basic_salary * 0.4) + (medical) - (basic_salary * 0.1) - (insurance) - (basic_salary * 0.1));
+    double transport = basic_salary * 0.2;
+    double housing = basic_salary * 0.4;
+    double retirement_401k = basic_salary * 0.1;
+    double income_tax = basic_salary * 0.1;
+    
+    //Gross salary = Basic salary + Trans Allow + Housing allowanc + med allow - 401k - insurance - income tax
+    gross_salary = basic_salary + transport + housing + medical - retirement_401k - insurance - income_tax;
 
     int gross_salary_integer = gross_salary;
     printf("The Gross Salary is %d", gross_salary_integer);
