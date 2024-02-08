36ebf8b6b20ba25d4da6032b73fb0d182f2f0fcb
Kooji707 <bboykooji@gmail.com>
2024-02-07 22:27:47 -0800

    Whitespace deleted

--- week3/float_to_dollars.c
+++ week3/float_to_dollars.c
@@ -5,13 +5,9 @@ int main (){
     double number_a, money_dollars, money_cents;
     printf("Enter your value with decimals: \n");
     scanf("%lf",&number_a);
-
     money_cents = modf(number_a, &money_dollars);
-
     printf("%.0f dollars", money_dollars);
-
     money_cents = money_cents * 100;
-
     printf(" and %.0f cents.", money_cents);
 
     return 0;
