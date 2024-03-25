35f11057879065065f08ed113f218625c4861bc4
Kooji707 <bboykooji@gmail.com>
2024-03-25 11:36:36 -0700

    improved

--- week9/bubbleimprove.c
+++ week9/bubbleimprove.c
@@ -1,32 +1,31 @@
 #include <stdio.h>
 
 void bubbleSort(int array[], int length) {
-    for (int i = 0; i < length - 1; i++)
-    {
-        for (int j = 0; j < length - i - 1; j++)
-        {
-            if (array[j] > array[j+1])
-            {
+    int passesCount = 0;
+    for (int i = 0; i < length - 1; i++) {
+        for (int j = 0; j < length - i - 1; j++) {
+            if (array[j] > array[j+1]) {
                 int temp = array[j];
                 array [j] = array[j+1];
                 array [j+1] = temp;
+                passesCount++;
             }
-            
         }
-        
+        if (passesCount == 0) {
+            break;
+        }
     }
 }
 
 void printArray(int array[], int length) {
-    for (int i = 0; i < length; i++)
-    {
+    for (int i = 0; i < length; i++) {
         printf("%d ", array[i]);
     }
-    
+    printf("\n");
 }
 
-int main (){
-    int array[] = {5,4,6,7,9,1,3,2};
+int main () {
+    int array[] = {20,1,11,12,99,32,4,2};
     int length = sizeof(array) / sizeof(array[0]);
 
     bubbleSort(array, length);
